c794a8761961f1086b2f19b34243507697a120a3
Priscila Roza <priscilarozasilva@gmail.com>
2021-10-07 17:17:10 -0300

    feat(setting the theme)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../data/ui/styles/globals.css';
-import type { AppProps } from 'next/app'
-
+import "../data/ui/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import theme from "../data/ui/themes/theme";
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>E-Diaristas</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        {/* eslint-disable */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/fonts/tw-icons/css/treinaweb-icons.css" rel="stylesheet" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
